59cfb677cb8aa4025929daeb8a4c4b788830dff0
Mian56 <ahmad.amy560@gmail.com>
2024-04-09 00:06:38 -0400

    GarmentFactory

--- src/main/java/org/example/GarmentFactory.java
+++ src/main/java/org/example/GarmentFactory.java
@@ -4,16 +4,16 @@ package org.example;
 //// Factory class responsible for creating garments
 public class GarmentFactory {
 
-   // Method to create garment based on type and variant
-   public Garment createGarment(String type, String variant){
+   // Method to create garment based on type and design
+   public Garment createGarment(String type, String design){
       // Check the type of garment requested
       if (type.equalsIgnoreCase("Top")){
-         return new Top(variant);
+         return new Top(design);
       }
       else if(type.equalsIgnoreCase("Pant")){
-         return new Pant(variant);
+         return new Pant(design);
       } else if (type.equalsIgnoreCase("shoe")) {
-         return new Shoe(variant);
+         return new Shoe(design);
       }
 
       System.out.println("Invalid garment type: " + type);
